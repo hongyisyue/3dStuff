13170a1c6b6fc22c4b0f1b330e833ca4fdd75aec
Hongyi Syue <homeyxue@outlook.com>
2021-11-03 20:23:56 -0600

    initial set up

---
+++ react-3d/src/App.js
@@ -0,0 +1,24 @@
+import { Suspense } from 'react';
+import styled from 'styled-components';
+import './App.css';
+import { Earth } from './components/earth';
+import { Canvas } from '@react-three/fiber';
+
+const CanvasContainer = styled.div`
+  width: 100%;
+  height: 100%
+`;
+
+function App() {
+  return (
+    <CanvasContainer>
+      <Canvas>
+        <Suspense fallback={null}>
+          <Earth/>
+        </Suspense>
+      </Canvas>
+    </CanvasContainer>
+  );
+}
+
+export default App;
