b140326eb17710b7031357b5a320c8f3217b86b0
Hongyi Syue <homeyxue@outlook.com>
2021-11-21 21:47:15 -0600

    rotate earth by mouse, starts bg

--- react-3d/src/components/earth/index.jsx
+++ react-3d/src/components/earth/index.jsx
@@ -1,22 +1,66 @@
 import { useLoader } from "@react-three/fiber";
-import React from "react";
+import React, { useRef, PointerEvent } from "react";
 import * as THREE from "three";
 import EarthDayMap from "../../assets/textures/8k_earth_daymap.jpg"
 import EarthNightMap from "../../assets/textures/8k_earth_nightmap.jpg"
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
+import { OrbitControls, Stars } from "@react-three/drei";
 
 export function Earth(params) {
     const [dayMap, nightMap, normalMap, specularMap, cloudsMap] = useLoader(
         THREE.TextureLoader,
         [EarthDayMap, EarthNightMap, EarthNormalMap, EarthSpecularMap, EarthCloudsMap]
     );
+
+
+  const earthRef = useRef();
+  const cloudsRef = useRef();
+  let isEnter = false;
+  let isPressing = false;
+
+  function toggleEnter(v) {
+    isEnter = v;
+  }
+  function togglePress(v) {
+    isPressing = v;
+  }
+  function rotate(e) {
+    if (isEnter && isPressing) {
+        earthRef.current.rotation.y -= e.width * 0.03;
+        cloudsRef.current.rotation.y -= e.width * 0.03;
+    }
+  }
     return (
         <>
-            <pointLight color="#f6f3ea" position={[2, 0, 5]} intensity={1.2}/>
-            <mesh position={[0, 0, 3]}>
-                <sphereGeometry args={[1, 32, 32]}/>
+            <pointLight color="#f6f3ea" position={[0, 0, 10]} intensity={10} />
+            <Stars
+                radius={300}
+                depth={60}
+                count={20000}
+                factor={7}
+                saturation={0}
+                fade={true}
+            />
+            <mesh ref={cloudsRef} position={[0, 0, 0]}
+                onPointerEnter={(e) => {
+                    toggleEnter(true);
+                }}
+                onPointerDown={(e) => {
+                    togglePress(true);
+                }}
+                onPointerMove={(e) => {
+                    rotate(e);
+                }}
+                onPointerUp={(e) => {
+                    togglePress(false);
+                }}
+                onPointerLeave={(e) => {
+                    toggleEnter(false);
+                }}
+            >
+                <sphereGeometry args={[1, 32, 32]} />
                 <meshPhongMaterial
                     map={cloudsMap}
                     opacity={0.4}
@@ -25,7 +69,7 @@ export function Earth(params) {
                     side={THREE.DoubleSide}
                 />
             </mesh>
-            <mesh position={[0, 0, 3]}>
+            <mesh ref={earthRef} position={[0, 0, 0]}>
                 <sphereGeometry args={[1, 32, 32]} />
                 <meshPhongMaterial specularMap={specularMap} />
                 <meshStandardMaterial
@@ -34,6 +78,7 @@ export function Earth(params) {
                     metalness={0.4}
                     roughness={0.7}
                 />
+                {/* <OrbitControls enablePan={false} enableZoom={false} enableRotate={true} /> */}
             </mesh>
         </>
 
