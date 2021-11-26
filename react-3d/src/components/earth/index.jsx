528abd670894459e87e5e0eef30b83d071b625fe
Hongyi Syue <homeyxue@outlook.com>
2021-11-26 00:33:01 -0600

    xiamen loca

--- react-3d/src/components/earth/index.jsx
+++ react-3d/src/components/earth/index.jsx
@@ -6,7 +6,7 @@ import EarthNightMap from "../../assets/textures/8k_earth_nightmap.jpg"
 import EarthNormalMap from "../../assets/textures/8k_earth_normal_map.jpg";
 import EarthSpecularMap from "../../assets/textures/8k_earth_specular_map.jpg";
 import EarthCloudsMap from "../../assets/textures/8k_earth_clouds.jpg";
-import { OrbitControls, Stars } from "@react-three/drei";
+import { OrbitControls, Stars, Sphere } from "@react-three/drei";
 
 export function Earth(params) {
     const [dayMap, nightMap, normalMap, specularMap, cloudsMap] = useLoader(
@@ -15,62 +15,73 @@ export function Earth(params) {
     );
 
 
-  const earthRef = useRef();
-  const cloudsRef = useRef();
-  let isEnter = false;
-  let isPressing = false;
+    const earthRef = useRef();
+    const cloudsRef = useRef();
+    let isEnter = false;
+    let isPressing = false;
 
-  let lastX;
-  let lastY;
+    let lastX;
+    let lastY;
 
-  useFrame(({ clock }) => {
-    const elapsedTime = clock.getElapsedTime();
-
-    earthRef.current.rotation.y = elapsedTime / 6;
-    cloudsRef.current.rotation.y = elapsedTime / 6;
-  });
-
-  function toggleEnter(e) {
-    if (isEnter) {
-        isEnter = !isEnter;
-        lastX = undefined;
-        lastY = undefined;
-    } else {
-        isEnter = true
-        lastX = e.width;
-        lastY = e.height;
+    const xiamen = {
+        lat: 24.4797 * Math.PI / 180,
+        lng: 118.0818 * Math.PI / 180 + 125 * Math.PI / 180,
     }
-  }
 
-  function togglePress(e) {
-    if (isPressing) {
-        console.log('up');
-        isPressing = !isPressing;
-        lastX = undefined;
-        lastY = undefined;
-    } else {
-        console.log('down -> lastX');
-        isPressing = true
-        lastX = e.width;
-        lastY = e.height;
-        console.log(lastX);
+    const xiamen_xyz = {
+        x: Math.cos(xiamen.lng) * Math.cos(xiamen.lat),
+        y: Math.sin(xiamen.lng) * Math.cos(xiamen.lat),
+        z: Math.sin(xiamen.lat)
     }
-  }
-  function rotate(e) {
-    if (isPressing) {
-        console.log('drag: ' + e.width);
-        if (e.width < lastX) {
-            earthRef.current.rotation.y -= e.width * 0.08;
-            cloudsRef.current.rotation.y -= e.width * 0.08;
+
+    // useFrame(({ clock }) => {
+    //     const elapsedTime = clock.getElapsedTime();
+
+    //     earthRef.current.rotation.y = elapsedTime / 6;
+    //     cloudsRef.current.rotation.y = elapsedTime / 6;
+    // });
+
+    //   function toggleEnter(e) {
+    //     if (isEnter) {
+    //         isEnter = !isEnter;
+    //         lastX = undefined;
+    //         lastY = undefined;
+    //     } else {
+    //         isEnter = true
+    //         lastX = e.width;
+    //         lastY = e.height;
+    //     }
+    //   }
+
+      function togglePress(e) {
+        if (isPressing) {
+            console.log('up');
+            isPressing = !isPressing;
+            lastX = undefined;
+            lastY = undefined;
         } else {
-            earthRef.current.rotation.y += e.width * 0.08;
-            cloudsRef.current.rotation.y += e.width * 0.08;
+            console.log('down -> lastX');
+            isPressing = true
+            lastX = e.width;
+            lastY = e.height;
+            console.log(lastX);
         }
-    }
-  }
+      }
+      function rotate(e) {
+        if (isPressing) {
+            console.log('drag: ' + e.width);
+            if (e.width < lastX) {
+                earthRef.current.rotation.y -= e.width * 0.03;
+                cloudsRef.current.rotation.y -= e.width * 0.03;
+            } else {
+                earthRef.current.rotation.y += e.width * 0.03;
+                cloudsRef.current.rotation.y += e.width * 0.03;
+            }
+        }
+      }
     return (
         <>
-            <pointLight color="#f6f3ea" position={[-5, 10, 10]} intensity={20} />
+            <pointLight color="#f6f3ea" position={[-25, 50, 50]} intensity={6} />
             <Stars
                 radius={300}
                 depth={60}
@@ -80,36 +91,44 @@ export function Earth(params) {
                 fade={true}
             />
             <mesh ref={cloudsRef} position={[0, 0, 0]}
-                // onPointerDown={(e) => {
-                //     togglePress(e);
-                // }}
-                // onPointerMove={(e) => {
-                //     rotate(e);
-                // }}
-                // onPointerUp={(e) => {
-                //     togglePress(e);
-                // }}
+            onPointerDown={(e) => {
+                togglePress(e);
+            }}
+            onPointerMove={(e) => {
+                rotate(e);
+            }}
+            onPointerUp={(e) => {
+                togglePress(e);
+            }}
+            onPointerLeave={(e) => {
+                togglePress(e);
+            }}
             >
-                <sphereGeometry args={[1, 32, 32]} />
+                <sphereGeometry args={[1.025, 64, 64]} />
                 <meshPhongMaterial
                     map={cloudsMap}
-                    opacity={0.4}
+                    opacity={0.2}
                     depthWrite={true}
                     transparent={true}
                     side={THREE.DoubleSide}
                 />
             </mesh>
             <mesh ref={earthRef} position={[0, 0, 0]}>
-                <sphereGeometry args={[1, 32, 32]} />
-                <meshPhongMaterial specularMap={specularMap} />
+                <sphereGeometry args={[1, 64, 64]} />
+                <meshPhongMaterial specularMap={specularMap}/>
                 <meshStandardMaterial
-                    map={nightMap}
+                    map={dayMap}
                     normalMap={normalMap}
                     metalness={0.4}
                     roughness={0.7}
                 />
                 {/* <OrbitControls enablePan={false} enableZoom={false} enableRotate={true} /> */}
+            <mesh position={[xiamen_xyz.x, xiamen_xyz.z, xiamen_xyz.y]}>
+                <sphereBufferGeometry args={[0.0125, 32, 32]}/>
+                <meshBasicMaterial color="red"></meshBasicMaterial>
             </mesh>
+            </mesh>
+
         </>
 
     );
